e8129c4e60d4db5d8a9be04e03d5cc6194e9f218
Son-Gyeongi <thsruddl77@gmail.com>
2024-09-30 02:43:37 +0900

    chore: 백엔드 개발에 필요한 최소한의 HTML 지식

--- hanbit/src/main/java/kr/co/hanbit/SimpleRestController.java
+++ hanbit/src/main/java/kr/co/hanbit/SimpleRestController.java
@@ -1,6 +1,7 @@
 package kr.co.hanbit;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,12 @@ public class SimpleRestController {
     @RequestMapping("/bye")
     public String bye() {
         return "Bye";
+    }
+
+    // html - form 태그의 input 태그에 입력된 값을 어떻게 서버로 보내고 받는지 확인
+    @RequestMapping("/article")
+    public String createArticle(@RequestParam("title") String title,
+                                @RequestParam("content") String content) {
+        return String.format("title=%s / content=%s", title, content);
     }
 }
