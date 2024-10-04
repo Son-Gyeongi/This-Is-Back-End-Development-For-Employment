eb25ff0a7793ed1a8818f68d84477e904e345282
Son-Gyeongi <thsruddl77@gmail.com>
2024-10-04 15:27:59 +0900

    init setting

---
+++ management/src/main/java/kr/co/hanbit/product/management/ManagementApplication.java
@@ -0,0 +1,13 @@
+package kr.co.hanbit.product.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementApplication.class, args);
+    }
+
+}
