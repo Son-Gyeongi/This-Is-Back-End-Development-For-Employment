41322b1d9268c86668ac17343d4c487accaa1bbf
Son-Gyeongi <thsruddl77@gmail.com>
2024-12-08 19:47:08 +0900

    init setting

---
+++ shortenurlservice_practice/src/main/java/kr/co/shortenurlservice_practice/ShortenurlservicePracticeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.shortenurlservice_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenurlservicePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenurlservicePracticeApplication.class, args);
+	}
+
+}
---
+++ shortenurlservice_practice/src/test/java/kr/co/shortenurlservice_practice/ShortenurlservicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.shortenurlservice_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenurlservicePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
